f72658de04d7517b40026a639ec60f833d1c48e1
Jessica Gomes <J.gomesss@outlook.com>
2025-03-06 12:27:24 -0300

    estilizando página planos

--- src/components/planos/listaplanos/ListaPlanosAdm.tsx
+++ src/components/planos/listaplanos/ListaPlanosAdm.tsx
@@ -11,65 +11,64 @@ import CardPlano1 from "../cardplanos/CardPlanos";
 import { ToastAlerta } from "../../../utils/ToastAlerta";
 
 function ListaPlanosAdm() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [planos, setPlanos] = useState<Plano[]>([]);
 
-    const [planos, setPlanos] = useState<Plano[]>([]);
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
 
-    const { usuario, handleLogout } = useContext(AuthContext);
-    const token = usuario.token;
-
-    async function buscarPlanos() {
-        try {
-            await buscar('/planos', setPlanos, {
-                headers: {
-                    Authorization: token,
-                },
-            })
-
-        } catch (error: any) {
-            if (error.toString().includes('403')) {
-                handleLogout()
-            }
-        }
+  async function buscarPlanos() {
+    try {
+      await buscar("/planos", setPlanos, {
+        headers: {
+          Authorization: token,
+        },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("403")) {
+        handleLogout();
+      }
     }
+  }
 
-    useEffect(() => {
-        if (token === '') {
-            ToastAlerta("Você precisa estar logado!", "info")
-            navigate('/login');
-        }
-    }, [token])
+  useEffect(() => {
+    if (token === "") {
+      ToastAlerta("Você precisa estar logado!", "info");
+      navigate("/login");
+    }
+  }, [token]);
 
-    useEffect(() => {
-        buscarPlanos()
-    }, [planos.length])
+  useEffect(() => {
+    buscarPlanos();
+  }, [planos.length]);
 
-    return (
-        <>
-            {planos.length === 0 && (
-                <DNA
-                    visible={true}
-                    height="200"
-                    width="200"
-                    ariaLabel="dna-loading"
-                    wrapperStyle={{}}
-                    wrapperClass="dna-wrapper mx-auto"
-                />
-            )}
-            <div className="flex justify-center w-full my-4">
-                <div className="container flex flex-col mx-2">
-                    <div className='container mx-auto my-4 
-                        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
-                    >
-                        {planos.map((plano) => (
-                            <CardPlano1 key={plano.id} plano={plano} />
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      {planos.length === 0 && (
+        <DNA
+          visible={true}
+          height="200"
+          width="200"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper mx-auto"
+        />
+      )}
+      <div className="flex justify-center w-full my-4">
+        <div className="container flex flex-col mx-2">
+          <div
+            className="container mx-auto my-4 
+                        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
+          >
+            {planos.map((plano) => (
+              <CardPlano1 key={plano.id} plano={plano} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ListaPlanosAdm;
